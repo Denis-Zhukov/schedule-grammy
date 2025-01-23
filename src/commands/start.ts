1d3885639054e347a1f83cbb3e3e43559c74245b
Denis Zhukov <deniszhukov.hachiko@gmail.com>
2025-01-23 20:56:05 +0300

    feat: user in db is creates
    feat: logs is downloadable

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,9 +1,26 @@
 import { Context } from 'grammy';
 
+import { prisma } from '@/utils/prisma-client';
+
 const start = async (ctx: Context) => {
-  const { first_name, last_name, username } = ctx.from ?? {};
+  const { id, first_name, last_name, username } = ctx.from ?? {};
 
   const name = first_name ?? last_name ?? username ?? 'Пользователь';
+
+  await prisma.user.upsert({
+    where: { id },
+    update: {
+      username,
+      surname: last_name,
+      name: first_name,
+    },
+    create: {
+      id: id!,
+      username,
+      surname: last_name,
+      name: first_name,
+    },
+  });
 
   await ctx.reply(
     `Привет, *${name}* 🤗\nДанный бот предназначен помочь вам с расписанием в школе СШ\\-27`,
--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,5 @@ dotenv.config();
 
 export const config = {
   API_TOKEN: process.env.API_TOKEN ?? '',
-  ADMIN_ID: process.env.ADMIN_ID ?? '',
+  ADMIN_ID: parseInt(process.env.ADMIN_ID ?? ''),
 } as const;
