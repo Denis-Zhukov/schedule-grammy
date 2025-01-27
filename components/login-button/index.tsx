0a2f5d0d97c652ca8a912e05802ac766873aadec
Denis Zhukov <deniszhukov.hachiko@gmail.com>
2025-01-27 14:39:08 +0300

    feat: username bot is public env

--- components/login-button/index.tsx
+++ components/login-button/index.tsx
@@ -2,15 +2,17 @@ import { config } from '@bot/config';
 import { LoginButton as TelegramLoginButton } from '@telegram-auth/react';
 import { signIn, SignInAuthorizationParams } from 'next-auth/react';
 
-export const LoginButton = () => (
-  <TelegramLoginButton
-    botUsername={config.USERNAME_BOT}
-    onAuthCallback={(data) => {
-      signIn(
-        'telegram-login',
-        {},
-        data as unknown as SignInAuthorizationParams,
-      );
-    }}
-  />
-);
+export const LoginButton = () => {
+  return (
+    <TelegramLoginButton
+      botUsername={config.USERNAME_BOT}
+      onAuthCallback={(data) => {
+        signIn(
+          'telegram-login',
+          {},
+          data as unknown as SignInAuthorizationParams,
+        );
+      }}
+    />
+  );
+};
