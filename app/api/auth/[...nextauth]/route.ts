e270425162830562d80a2fdca08bd7cf8bf1f4da
Denis Zhukov <deniszhukov.hachiko@gmail.com>
2025-01-28 10:36:27 +0300

    feat: login button

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,5 +4,4 @@ import { authOptions } from '@/config/next-auth';
 
 const handler = NextAuth(authOptions);
 
-export const GET = handler;
-export const POST = handler;
+export { handler as GET, handler as POST };
