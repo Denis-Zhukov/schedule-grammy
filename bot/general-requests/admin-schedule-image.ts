f9122ef162dff3a120bcea956819fea36c753385
Denis Zhukov <deniszhukov.hachiko@gmail.com>
2025-02-08 03:07:17 +0300

    feat: optimize send schedule images

--- bot/general-requests/admin-schedule-image.ts
+++ bot/general-requests/admin-schedule-image.ts
@@ -1,9 +1,12 @@
 import { InputFile } from 'grammy';
+import { readFileSync } from 'fs';
 import { resolve } from 'path';
 
 import { CustomContext } from '@bot/types';
 
+const fileBuffer = readFileSync(resolve('files', 'admin-schedule.jpg'));
+const file = new InputFile(fileBuffer);
+
 export const adminScheduleImage = async (ctx: CustomContext) => {
-  const file = new InputFile(resolve('files', 'admin-schedule.jpg'));
   await ctx.replyWithPhoto(file);
 };
